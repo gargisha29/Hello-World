b9afce806d676440f3fe638599fab37c228934ec
gargisha29 <52344738+gargisha29@users.noreply.github.com>
2019-06-29 20:10:10 +0530

    Update Rolling Dice.py

--- Rolling Dice.py
+++ Rolling Dice.py
@@ -1,11 +1,17 @@
 import random
 #help(random)
-play= random.randrange(2,7)
-Chance = input( " enter y to play, and Stop to End the Game: ")
- if Chance = "y" :
-  print( "you rolled ", play )
- else
-  print( " You had Stopped the Game")
+min=2
+max= 7
 
-      
+play = True
+while play:
+ chance = input( " enter y to play, and Stop to End the Game: ")
+ if chance =='y':
+    roll= random.randint(min,max)
+    print( "you rolled ", roll)
+  
+ else :
+    play = False
+    print( " You had Stopped the Game")
+
     
